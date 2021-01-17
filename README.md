5770c9821688848392e14a9307d7478292a7ba67
Qiwen Hua <hqw@huaqiwen.com>
2021-01-16 19:11:46 -0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# image-repository
+An image repository 🖼️ created using React and Google Firebase.
---
+++ src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>Image Repository</h1>
+      <h2>Your Image Repository</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+  )
+}
+
+export default Title;
